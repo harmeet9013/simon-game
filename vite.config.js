dac5b2176143b80d6a686b2601194992d0cff362
Harmeet Singh <evildoremon@gmail.com>
2023-06-10 19:15:41 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,9 @@ import react from "@vitejs/plugin-react-swc";
 export default defineConfig({
     plugins: [react()],
     base: "./",
+    resolve: {
+        extensions: [".jsx", ".json", ".mp3"],
+    },
     build: {
         rollupOptions: {
             output: {
