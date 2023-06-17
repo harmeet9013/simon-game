369cc719a6a38b95d87219243a15bd6d25c0d43a
Harmeet Singh <evildoremon@gmail.com>
2023-06-17 14:35:49 +0530

    Few changes.
    Removed unnecessary code + Added a new link to Source Code.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Heading from "./components/Heading";
 import Grids from "./components/Grids";
+import { OpenInNew } from "@mui/icons-material";
 
 export default function App() {
     const [level, setLevel] = useState(0);
@@ -39,7 +40,16 @@ export default function App() {
                 The website has a simple and colorful design that appeals to
                 both children and adults.
             </p>
-            <h3 className="created-by">by Harmeet Singh</h3>
+            <div className="footer">
+                <p>by Harmeet Singh</p>
+                <a
+                    href="https://github.com/harmeet9013/simon-game/"
+                    target="_blank"
+                >
+                    Source Code
+                    <OpenInNew sx={{ marginY: "-4px", marginX: "2px" }} />
+                </a>
+            </div>
         </div>
     );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -4,24 +4,4 @@ import "./index.css";
 
 createRoot(document.getElementById("root")).render(<App />);
 
-export const styles = {
-    box: {
-        display: "grid",
-        width: 200,
-        height: 200,
-        backgroundColor: "",
-        marginBottom: "70px",
-        marginRight: "70px",
-        border: "10px solid black",
-        borderRadius: "50px",
-        transition: "all 30ms ease-out",
-        ":hover": {
-            backgroundColor: "",
-        },
-        ":active": {
-            transform: "scale(0.9)",
-        },
-    },
-};
-
 export const inputs = ["red", "blue", "green", "yellow"];
